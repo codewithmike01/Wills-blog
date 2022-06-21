bf0af0a143d0ac9f059e45aaf13c4d84b16023aa
ginohmk <michotall95@gmail.com>
2022-06-21 19:43:54 +0100

    initial react app setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import EntertainmentPosts from './components/Posts/EntertainmentPosts';
+import TechPosts from './components/Posts/TechPosts';
+import PoliticsPosts from './components/Posts/PoliticsPosts';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="entertainment" element={<EntertainmentPosts />} />
+        <Route path="technology" element={<TechPosts />} />
+        <Route path="politics" element={<PoliticsPosts />} />
+      </Routes>
     </div>
   );
 }
