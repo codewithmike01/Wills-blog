d697a04b95a6240ca5e0712c414c21991c908dc8
ginohmk <michotall95@gmail.com>
2022-08-17 21:44:11 +0100

    initailize firebase

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDUJYivilxv7cdqffYIlMFghM9Dq0CJQAg',
+  authDomain: 'willsblog-97e1f.firebaseapp.com',
+  projectId: 'willsblog-97e1f',
+  storageBucket: 'willsblog-97e1f.appspot.com',
+  messagingSenderId: '600430174222',
+  appId: '1:600430174222:web:6fcad3b526f4362bdd9c5f',
+  measurementId: 'G-ZFRJJ5Q8CW',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
