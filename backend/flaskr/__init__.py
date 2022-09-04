ee6dc6a777b5ecec4f4f78e8d59dff6266feae16
ginohmk <michotall95@gmail.com>
2022-09-04 15:30:16 +0100

    Add Endpoint

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -22,9 +22,42 @@ def create_app(test_config=None):
         return response
 
 
+    @app.route('/users', methods=['POST'])
+    def create_users():
+        body = request.get_json()
+        new_first_name = body.get('first_name',None)
+        new_last_name = body.get('last_name',None)
+        new_nick_name = body.get('nick_name', None)
+        new_disable_account = body.get('disable_account', None)
+        new_facebook_link = body.get('facebook_link',None)
+        new_linkedIn_link = body.get('linkedIn_link',None)
+        new_instagram_link = body.get('instagram_link', None)
+        new_github_link = body.get('github_link',None)
+        new_role = body.get('role',None)
 
 
+        try:
+            user = User(
+                first_name = new_first_name,
+                last_name = new_last_name,
+                nick_name= new_nick_name,
+                disable_account= new_disable_account,
+                facebook_link= new_facebook_link,
+                linkedIn_link= new_linkedIn_link,
+                instagram_link= new_instagram_link,
+                github_link= new_github_link,
+                role = new_role
+            )
 
-      
-    
+            user.insert()
+
+            return jsonify({
+                'success': True,
+                'user': user.format()
+            })
+
+        except:
+            abort(404)
+
+
     return app
