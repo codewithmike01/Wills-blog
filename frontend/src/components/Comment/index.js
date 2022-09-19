1deaf1e6c3047e843da0381d370a86e185e11040
ginohmk <michotall95@gmail.com>
2022-09-19 20:49:28 +0100

    Add banner styles

--- frontend/src/components/Comment/index.js
+++ frontend/src/components/Comment/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
+import BannerContainer from '../Banner.js/banner.style';
 import BlogImage from '../../assets/blogImg.jpg';
 
 function index() {
   const [cookie] = useCookies(['user']);
   const navigate = useNavigate();
   return (
-    <Container className="flex column">
+    <BannerContainer className="flex column">
       <div className="comment-count opacity"> 1 Comment </div>
       <div className="comments">
         <div className="comment flex">
@@ -44,88 +44,8 @@ function index() {
           </button>
         </div>
       )}
-    </Container>
+    </BannerContainer>
   );
 }
 
 export default index;
-
-const Container = styled.div`
-  gap:2rem;
-
-  .comment-count{
-    font-size:1.5rem;
-  }
-  .comments{
-
-    .comment{
-      gap:1rem;
-      align-items:center;
-
-      .image-container{
-        width: 100px;
-        height: 100px;
-        border-radius:5px;
-      
-        img{
-          width:100%;
-          height: 100%
-          object-fit:cover;
-        }
-      }
-      
-        .comment-info {
-          align-items:center;
-          p{
-            font-size:0.92rem;
-          }
-          .user-name{
-            margin-bottom:-20px;
-            align-items:center;
-            gap:20px;
-
-            span{
-              font-size:0.8rem;
-            }
-          }
-          
-        }
-    }
-  }
-
-  form{
-    gap:20px;
-
-    p{
-      font-size:1.3rem;
-    }
-    .text-comment{
-      border: 1px solid #666;
-      padding: 10px 5px 10px 10px ;
-      width : 100%;
-      height: 200px;
-      background-color:transparent;
-      color:#fff;
-
-      &:focus{
-      outline:none;
-      }
-    }
-  }
-  
-  button {
-      width: 150px;
-      border: none;
-      color: #fff;
-      background-color: #222;
-      height: 40px;
-      font-size: 0.9rem;
-      cursor: pointer;
-      border-radius: 5px;
-      transition: background-color 0.5s ease-in-out;
-
-      &:hover {
-        background-color: #ffa301;
-      }
-    }
-    `;
