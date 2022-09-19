aa952080ac0167469b5a4595ddf1b2e4ea478fd6
ginohmk <michotall95@gmail.com>
2022-09-19 20:54:36 +0100

    Fix banner styled error in styled component

--- frontend/src/components/Banner.js/banner.style.js
+++ frontend/src/components/Banner.js/banner.style.js
@@ -1,83 +1,81 @@
 import styled from 'styled-components';
 
 const BannerContainer = styled.div`
-  gap:2rem;
-  .comment-count{
-    font-size:1.5rem;
+  gap: 2rem;
+
+  .comment-count {
+    font-size: 1.5rem;
   }
-  .comments{
+  .comments {
+    .comment {
+      gap: 1rem;
+      align-items: center;
 
-    .comment{
-      gap:1rem;
-      align-items:center;
-
-      .image-container{
+      .image-container {
         width: 100px;
         height: 100px;
-        border-radius:5px;
-      
-        img{
-          width:100%;
-          height: 100%
-          object-fit:cover;
+        border-radius: 5px;
+
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
         }
       }
-      
-        .comment-info {
-          align-items:center;
-          p{
-            font-size:0.92rem;
-          }
-          .user-name{
-            margin-bottom:-20px;
-            align-items:center;
-            gap:20px;
 
-            span{
-              font-size:0.8rem;
-            }
-          }
-          
+      .comment-info {
+        align-items: center;
+        p {
+          font-size: 0.92rem;
         }
+        .user-name {
+          margin-bottom: -20px;
+          align-items: center;
+          gap: 20px;
+
+          span {
+            font-size: 0.8rem;
+          }
+        }
+      }
     }
   }
 
-  form{
-    gap:20px;
+  form {
+    gap: 20px;
 
-    p{
-      font-size:1.3rem;
+    p {
+      font-size: 1.3rem;
     }
-    .text-comment{
+    .text-comment {
       border: 1px solid #666;
-      padding: 10px 5px 10px 10px ;
-      width : 100%;
+      padding: 10px 5px 10px 10px;
+      width: 100%;
       height: 200px;
-      background-color:transparent;
-      color:#fff;
+      background-color: transparent;
+      color: #fff;
 
-      &:focus{
-      outline:none;
+      &:focus {
+        outline: none;
       }
     }
   }
-  
-  button {
-      width: 150px;
-      border: none;
-      color: #fff;
-      background-color: #222;
-      height: 40px;
-      font-size: 0.9rem;
-      cursor: pointer;
-      border-radius: 5px;
-      transition: background-color 0.5s ease-in-out;
 
-      &:hover {
-        background-color: #ffa301;
-      }
+  button {
+    width: 150px;
+    border: none;
+    color: #fff;
+    background-color: #222;
+    height: 40px;
+    font-size: 0.9rem;
+    cursor: pointer;
+    border-radius: 5px;
+    transition: background-color 0.5s ease-in-out;
+
+    &:hover {
+      background-color: #ffa301;
     }
-  
+  }
 `;
 
 export default BannerContainer;
